ba2f6eddc4be3b37662ca601d0bb26e12ca9c28e
Brad Palagi <brad.palagi@grianger.com>
2025-12-01 22:01:00 -0600

    day1.py

--- day1.py
+++ day1.py
@@ -6,7 +6,7 @@ def calcChange(val):
         return -int(val[1:])
     return int(val[1:])
 
-def main(session_cookie):
+def landOnZero(session_cookie):
     response = requests.get('https://adventofcode.com/2025/day/1/input', cookies={'session': session_cookie})
     input = response.text.strip().split('\n')
 
@@ -21,8 +21,33 @@ def main(session_cookie):
 
     return ret
 
+
+def passZero(session_cookie):
+    response = requests.get('https://adventofcode.com/2025/day/1/input', cookies={'session': session_cookie})
+    input = response.text.strip().split('\n')
+
+    count = 50
+    ret = 0
+
+    for i in input:
+        change = calcChange(i)
+
+        count += change
+
+        while count >= 100:
+            count -= 100
+            ret += 1
+        while count <= -1:
+            count += 100
+            ret += 1
+        if count == 0 and change > -100 and change < 100:
+            ret += 1
+        
+    return ret
+
+
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         print("Usage: python day1.py <session_cookie>")
         sys.exit(1)
-    print(main(sys.argv[1]))
+    print(passZero(sys.argv[1]))
